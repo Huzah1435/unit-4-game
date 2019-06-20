b955b3831bd7c1435d4c96e990095df5c61f7538
Thomas Bradford <Huzah1435@gmail.com>
2019-06-19 20:13:01 -0500

    finished game added music but couldnt figure out how to end it after ever win against defender

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,232 @@
-var Char1 = {
+var Char = []
+
+Char[0] = {
+
     name: "Kylo Ren",
+
     hitpoints: 120,
+
     attackpower: 8,
-    counterattackpower: 20 
+
+    counterattackpower: 20,
+
+    tag: "#char1"
+
 };
-var Char2 = {
+
+Char[1] = {
+
     name: "Jango Fett",
+
     hitpoints: 140,
+
     attackpower: 12,
-    counterattackpower: 30
+
+    counterattackpower: 20,
+
+    tag: "#char2"
+
 }
-var Char3 = {
+
+Char[2] = {
+
     name: "Anakin Skywalker",
+
     hitpoints: 160,
+
     attackpower: 16,
-    counterattackpower: 35
+
+    counterattackpower: 15,
+
+    tag: "#char3"
+
 }
-var Char4 = {
+
+Char[3] = {
+
     name: "Quigon-jin",
+
     hitpoints: 180,
+
     attackpower: 20,
-    counterattackpower: 40
+
+    counterattackpower: 10,
+
+    tag: "#char4"
+
 }
-$(document).ready(function () {
-    var taglist = ["#char1", "#char2", "#char3", "#char4"];
-    var tags = "";
+
+var taglist = ["#char1", "#char2", "#char3", "#char4"];
+
+var tags = "";
+
+var playerpicked;
+
+var enemypicked;
+
+var pindex;
+
+var dindex;
+
+var player;
+
+var defender;
+
+var gameover;
+
+var attackpts;
+
+var defendpts;
+
+var wins;
+
+var soundbyte;
+
+
+
+function gameinit() {
+
+    $("#char1_name").text(Char[0].name);
+
+    $("#char2_name").text(Char[1].name);
+
+    $("#char3_name").text(Char[2].name);
+
+    $("#char4_name").text(Char[3].name);
+
+    $("#char1_hp").text(Char[0].hitpoints);
+
+    $("#char2_hp").text(Char[1].hitpoints);
+
+    $("#char3_hp").text(Char[2].hitpoints);
+
+    $("#char4_hp").text(Char[3].hitpoints);
+
+
+
     tags = tags.concat(taglist[0], ", ", taglist[1], ", ", taglist[2], ", ", taglist[3]);
-    console.log(tags);
+
+
+
     playerpicked = false;
+
     enemypicked = false;
-    $(tags).click(function () {
-        if (!playerpicked) {  
+
+    gameover = false;
+
+    wins = 0;
+
+}
+
+gameinit();
+
+
+
+$(tags).click(function () {
+
+
+
+    if (!playerpicked) {
+
         $("#yourcharacter").append($(this))
-        player = "#"+this.id;
+
+        var p = "#" + this.id;
+
+        pindex = p.slice(-1) - 1;
+
+        player = Char[pindex];
+
         playerpicked = true;
-        for (i = 0; i < taglist.length; i++) {
-            if (taglist [i] != player){
+
+        attackpts = player.attackpower;
+
+        for (var i = 0; i < taglist.length; i++) {
+
+            if (taglist[i] != p) {
+
                 $("#enemies").append($(taglist[i]));
+
             }
+
         }
+
     }
-    else if (playerpicked && ("#"+this.id == player)){
+
+    else if (playerpicked && ("#" + this.id == player)) {
+
         console.log("playeralreadypicked")
+
     }
+
     else if (!enemypicked) {
+
         $("#defender").append(this);
-        defender = "#" +this.id;
+
+        var d = "#" + this.id;
+
+        dindex = d.slice(-1) - 1;
+
+        defender = Char[dindex];
+
+        defendpts = defender.counterattackpower;
+
         enemypicked = true;
+
     }
+
 });
-});
+
+$("#attack").click(function () {
+
+
+
+    if (playerpicked && enemypicked && !gameover) {
+
+        defender.hitpoints -= attackpts;
+
+        attackpts += player.attackpower;
+
+        console.log(defender.tag + "_hp");
+
+        $(defender.tag + "_hp").text(defender.hitpoints);
+
+
+
+        player.hitpoints -= defender.counterattackpower;
+
+        console.log(player.tag + "_hp");
+
+        $(player.tag + "_hp").text(player.hitpoints);
+
+    }
+
+    if (player.hitpoints < 1) {
+        console.log("you lost");
+        $(".message").text("You Lost The Game!")
+
+        location.reload(true);
+
+    }
+
+    if (defender.hitpoints < 1) {
+        document.getElementById('winnersound').play();
+        console.log("you won this battle");
+        $(".message").text("You Won The Battle!")
+        wins += 1;
+
+        $(defender.tag).empty();
+
+        enemypicked = false;
+
+    }
+
+    if (wins == 3) {
+        console.log("you won the game");
+        $(".message").text("You Won The Game!")
+
+
+    }
+
+
+
+})
